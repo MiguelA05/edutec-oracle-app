df939b073c1040fc2450b20514d58320eafb62c0
MiguelA05 <126737880+MiguelA05@users.noreply.github.com>
2025-05-09 20:11:57 -0500

    Update ConnectionOracle.java

--- src/main/java/org/uniquindio/repositorie/ConnectionOracle.java
+++ src/main/java/org/uniquindio/repositorie/ConnectionOracle.java
@@ -5,9 +5,9 @@ import java.sql.SQLException;
 
 public class ConnectionOracle {
 
-    private static final String URL = "jdbc:oracle:thin:@localhost:1521:xe"; // Cambia el SID si no usas XE
-    private static final String USUARIO = "HR";
-    private static final String CONTRASENA = "HR";
+    private static final String URL = "jdbc:oracle:thin:@localhost:1521:XE";
+    private static final String USUARIO = "SYSTEM";
+    private static final String CONTRASENA = "12345678";
 
     public static Connection conectar() throws SQLException {
         return DriverManager.getConnection(URL, USUARIO, CONTRASENA);
